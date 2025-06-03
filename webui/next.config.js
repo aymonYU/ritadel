8f6bdb0efac5b600a3da3a810428dbbf158e2e7b
aymonyu <aymonyu@tencent.com>
2025-06-03 09:20:21 +0800

    feat: 定义输出文件

--- webui/next.config.js
+++ webui/next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'dist',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
   experimental: {
     // Enable SWC for faster compilation
     forceSwcTransforms: true,
